c99ade8b0b65ef2ef88566e989114a9d538b38f2
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-06-08 18:22:17 +0530

    .

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -2,14 +2,15 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import "./style.css";
-
+document.body.style.background = ' #62B16E';
 export default function Footer() {
   return (
-    <nav className="navbar navbar-light bg-light fnav">
+    
+    <nav className="navbar navbar-light bg-info fnav">
       <div className="container-fluid tc">
         <div className="row">
           <div className="col">
-            <span>Aptra Technologies pvt Ltd. </span>
+            <span >Aptra Technologies pvt Ltd. </span>
           </div>
         </div>
       </div>
--- frontend/src/pages/Authentication/Login.js
+++ frontend/src/pages/Authentication/Login.js
@@ -56,17 +56,7 @@ const Login = () => {
 
         }
     };
-    color_1 = document.querySelector("#color-1");
-    color_2 = document.querySelector("#color-2");
-    body = document.querySelector("body");
-    
-    function changeBackgroundColor() {
-        body.style.backgroundImage = "linear-gradient(to right, " +color_1.value+ ", "+ color_2.value + ")";
-    }
-    
-    //************* Both the listeners work fine and the cashed selectors
-    color_1.addEventListener("input", changeBackgroundColor);
-    color_2.addEventListener("input", changeBackgroundColor);
+
     return (
         <>
             <Header />
